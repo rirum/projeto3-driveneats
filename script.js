75257bffc3af401c34159a6e992f12baa041f752
camille <c.miyashita@gmail.com>
2022-10-16 15:56:35 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-var valor = 0;
-
-function confirmacao(produto, preco) {
-    valor = valor + preco;
-    alert(valor);
-}
-
 
 
 
@@ -30,16 +23,43 @@ function botaoAtivo(){
     if(pratos.querySelector('.ativo') 
     && bebidas.querySelector('.ativo')
     && sobremesas.querySelector('.ativo')) {
-        document.querySelector('.button').classList.add("button-ativo");
-    document.querySelector('#button-text').innerHTML="Fechar pedido";
+        let botao = document.querySelector('.button');
+        botao.classList.add("button-ativo");
+        botao.onclick=fecharPedido;
+     document.querySelector('#button-text').innerHTML="Fechar pedido";
+
     }
 
 }
 
+function fecharPedido() {
+    let pratos = document.querySelector('.container-pratos');
+    let pratoSelecionado = pratos.querySelector('.ativo');
+    let bebidas = document.querySelector('.container-bebidas');
+    let bebidaSelecionado = bebidas.querySelector('.ativo');
+    let sobremesas = document.querySelector('.container-sobremesas');
+    let sobremesaSelecionado = sobremesas.querySelector('.ativo');
 
 
+    let nomePratoSelecionado = pratoSelecionado.getElementsByClassName('nome-prato')[0].innerHTML;
+    let valorPratoSelecionado = pratoSelecionado.getElementsByClassName('valor-prato')[0].getAttribute('value');
+    
+    let nomeBebidaSelecionado = bebidaSelecionado.getElementsByClassName('nome-prato')[0].innerHTML;
+    let valorBebidaSelecionado = bebidaSelecionado.getElementsByClassName('valor-prato')[0].getAttribute('value');
 
+    let nomeSobremesaSelecionado = sobremesaSelecionado.getElementsByClassName('nome-prato')[0].innerHTML;
+    let valorSobremesaSelecionado = sobremesaSelecionado.getElementsByClassName('valor-prato')[0].getAttribute('value');
 
+    
+    let valorTotal = Number(valorPratoSelecionado) + Number(valorBebidaSelecionado) + Number(valorSobremesaSelecionado);
 
+    let whatsApp = `Olá, gostaria de fazer o pedido:\n
+    - Prato: ${nomePratoSelecionado}
+    - Bebida: ${nomeBebidaSelecionado}
+    - Sobremesa: ${nomeSobremesaSelecionado}
+    - Total: ${valorTotal.toFixed(2)} `;
 
+    whatsApp = encodeURIComponent(whatsApp);
+    window.open("https://wa.me/5541999245138?text=" + whatsApp);
+}
 
